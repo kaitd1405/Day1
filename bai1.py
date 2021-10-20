9880b51881a91429f9145cafba7b8c24d4d5a62f
kaitd1405 <kaidoprime2002@gmail.com>
2021-10-20 21:32:35 +0700

    pull

---
+++ bai1.py
@@ -0,0 +1,4 @@
+import math
+x = int(input("Nhap ban kinh: "))
+print("S=",x*x*math.pi)
+print("C=",2*x*math.pi)
---
+++ bai2.py
@@ -0,0 +1,3 @@
+import math
+x=int(input("Nhap x: "))
+print("F(x)=", (x*x+math.exp(x)+math.sin(x))/math.sqrt(x*x+1))
